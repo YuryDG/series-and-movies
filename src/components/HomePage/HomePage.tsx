fc7cf0eda5528b4b87caee019dc01819bf7c3f99
Yury <yurydgfuentes@gmail.com>
2021-07-29 09:45:02 +0300

    Redirect to login page from home is user is not authenticated

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,7 +1,16 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { Redirect } from "react-router-dom";
+
 export const HomePage: React.FC = () => {
-  return (
-    <section className="home-page">
-      <h1>Movies and Series</h1>
-    </section>
-  )
+  const { isAuthenticated } = useAuth0();
+
+  if (!isAuthenticated) {
+    return <Redirect to="/" />
+  } else {
+    return (
+      <section className="home-page">
+        <h1>Movies and Series</h1>
+      </section>
+    )
+  }
 }
