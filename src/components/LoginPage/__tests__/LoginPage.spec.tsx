409884590549b4634127d8fc2b2e2ac2bd726b3f
Yury <yurydgfuentes@gmail.com>
2021-07-29 10:13:21 +0300

    add test for redirect in login page

---
+++ src/components/LoginPage/__tests__/LoginPage.spec.tsx
@@ -0,0 +1,20 @@
+import { render } from "@testing-library/react";
+import { createMemoryHistory } from "history";
+import { Router } from "react-router";
+import { LoginPage } from "../LoginPage";
+
+jest.mock('@auth0/auth0-react', () => ({
+  useAuth0: () => ({ isAuthenticated: true })
+}))
+
+describe('LoginPage', () => {
+  it("should redirect to home page", () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={history}>
+        <LoginPage />
+      </Router>,
+    );
+    expect(history.location.pathname).toBe("/");
+  });
+})
