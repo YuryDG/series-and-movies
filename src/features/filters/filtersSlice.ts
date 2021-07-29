3d3d1f0517c3493bc26e7a38e34cf5ee984ae7ef
Yury <yurydgfuentes@gmail.com>
2021-07-29 14:21:23 +0300

    add environment setting for items per page

--- src/features/filters/filtersSlice.ts
+++ src/features/filters/filtersSlice.ts
@@ -8,7 +8,7 @@ const initialState: FiltersState = {
   sortBy: '',
   pagination: {
     currentPage: 1,
-    itemsPerPage: 6
+    itemsPerPage: process.env.ITEMS_PER_PAGE ? +process.env.ITEMS_PER_PAGE : 6
   }
 };
 
@@ -18,15 +18,19 @@ export const filtersSlice = createSlice({
   reducers: {
     setYearFilter: (state, { payload }: PayloadAction<number>) => {
       state.year = payload;
+      state.pagination.currentPage = 1;
     },
     setSearchTermFilter: (state, { payload }: PayloadAction<string>) => {
       state.searchTerm = payload;
+      state.pagination.currentPage = 1;
     },
     setProgramFilter: (state, { payload }: PayloadAction<string>) => {
       state.type = payload;
+      state.pagination.currentPage = 1;
     },
     setSortFilter: (state, { payload }: PayloadAction<FiltersState['sortBy']>) => {
       state.sortBy = payload;
+      state.pagination.currentPage = 1;
     },
     clearFilters: (state) => {
       state = initialState;
