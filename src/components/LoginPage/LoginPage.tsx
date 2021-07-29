02252d6a6bf80e5b675e108f886c37c8def5ee63
Yury <yurydgfuentes@gmail.com>
2021-07-29 22:19:28 +0300

    add stiles to login page

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,5 +1,6 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { Redirect } from 'react-router-dom';
+import './LoginPage.css';
 
 export const LoginPage: React.FC = () => {
   const { loginWithRedirect, isAuthenticated } = useAuth0();
@@ -7,10 +8,23 @@ export const LoginPage: React.FC = () => {
     return <Redirect to="/home" />
   } else {
     return (
-      <div className="login-page">
-        <button onClick={() => loginWithRedirect()}>
-          Log In
-        </button>
+      <div className="container">
+        <div className="login-page">
+          <div className="image-container">
+            <img
+              src="./movies-and-series.png"
+              alt="Movies and Series"
+              width="300"
+              height="300"
+            />
+          </div>
+          <div className="btn-container">
+            <p>The New Netflix</p>
+            <button className="login-btn" onClick={() => loginWithRedirect()}>
+              LOGIN
+            </button>
+          </div>
+        </div>
       </div>
     )
   }
