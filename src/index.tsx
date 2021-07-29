3bcfd977a94a23759065f2ce6d36929558ae61d9
Yury <yurydgfuentes@gmail.com>
2021-07-29 09:41:25 +0300

    add Auth0Provider to the app

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,22 @@ import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import { Auth0Provider } from '@auth0/auth0-react';
 import * as serviceWorker from './serviceWorker';
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN || '';
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID || '';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
