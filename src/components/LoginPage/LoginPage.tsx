9f7d1322f5699f4d9927c7a6351e86cf2efffb94
Yury <yurydgfuentes@gmail.com>
2021-07-29 09:42:40 +0300

    redirect to home after authenticate

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,5 +1,18 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import { Redirect } from 'react-router-dom';
+
 export const LoginPage: React.FC = () => {
-  return (
-    <div>LoginPage</div>
-  )
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
+
+  if (isAuthenticated) {
+    return <Redirect to="/home" />
+  } else {
+    return (
+      <div className="login-page">
+        <button onClick={() => loginWithRedirect()}>
+          Log In
+        </button>
+      </div>
+    )
+  }
 }
